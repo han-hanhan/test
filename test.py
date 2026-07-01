cb6e22c1a61158b0c9d0301e9d9e90b423657408
hanhanhan <861529094@qq.com>
2026-07-01 16:12:51 +0800

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.subplot(1, 2, 1)
+y1 = np.array([1,2,3,4,5,6,7,8,9,10])
+plt.plot(y1,label = 'y1')
+plt.title('y1')
+plt.subplot(1, 2, 2)
+y2 = np.array([1,2,3,4,5,6,7,8,9,10][::-1])
+plt.plot(y2,label = 'y2')
+plt.title('y2')
+plt.legend(loc = 'best')
+plt.show()
